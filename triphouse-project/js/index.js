dbe8b29f9a88faed2efe1c49cc81146e4fc597c4
Merildew <73027852+Merildew@users.noreply.github.com>
2021-08-13 20:24:28 +0300

    added reviews-section. refactoring

--- triphouse-project/js/index.js
+++ triphouse-project/js/index.js
@@ -6,15 +6,36 @@ $(document).ready(function(){
         slidesToShow: 4,
         responsive:[
             {
-                breakpoint: 1024,
+                breakpoint: 1025,
                 settings: {
                     slidesToShow: 3
                 }
             },
             {
-                breakpoint: 768,
+                breakpoint: 769,
                 settings: {
                     slidesToShow: 2
+                }
+            }
+        ]
+    });
+});
+
+$(document).ready(function(){
+    $('.reviews-panel').slick({
+        slidesToShow: 3,
+        responsive:[
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
                 }
             }
         ]
